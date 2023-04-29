809348ff2ce53dd533ebffcf952273e392eb000a
Michelle Trejo <michelletgar@gmail.com>
2023-04-29 00:26:14 -0600

    feat: Add new JavaScript file for variables

---
+++ src/01-variables-const.js
@@ -0,0 +1,12 @@
+let firstname = 'Pepe';
+firstname = 'Marcus';
+
+const lastname = "Phoenix";
+const condition = true;
+
+if (condition) {
+    const lastname = "Dexter";
+    console.log(lastname);
+}
+
+console.log(`Hola mundo ${firstname} ${lastname}`);
